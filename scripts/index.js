aca1af672aa3dcef1ce8186b968ab8900846e6a8
ranny80 <rannyvuong80@gmail.com>
2022-03-26 14:24:05 -0400

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,9 @@
+const loaded = "Game succesfully loaded";
+
+// Notice that the game has been loaded
+console.log(loaded);
+
+// Clicking the start button when it loaded
 function clickStart() {
 	const buttonClick = document.getElementById("startButton");
 	buttonClick.innerHTML = "<center>Coming Soon!</center>";	
